9d9f3ee835538cf26a84e48ee8482507805ce437
RigzorStudent <88572838+RigzorStudent@users.noreply.github.com>
2022-06-29 13:35:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/TEWzBtWNJ/model.json', modelReady);
+}
+
+
+function modelReady() {
+    console.log("modelReady")
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) 
+        {
+            console.log(error)
+        }
+else
+        {
+            console.log(results)
+            r = Math.floor(Math.random() * 255) + 1;
+            g = Math.floor(Math.random() * 255) + 1;
+            b = Math.floor(Math.random() * 255) + 1;
+        }
+
+        document.getElementById("result_label").innerHTML='I can hear-'+results[0].label
+        document.getElementById("result_confidence").innerHTML='Accuracy-'+(results[0].confidence*100).toFixed(2)+"%"
+        
+     
+       document.getElementById("result_label").style.color="rgb("+r+ "," +g+ "," +b+ ")"
+      document.getElementById("result_confidence").style.color="rgb("+r+ "," +g+ "," +b+ ")"
+
+      label=results[0].label
+
+      img1=document.getElementById('alien1')
+      img2=document.getElementById('alien2')
+      img3=document.getElementById('alien3')
+      img4=document.getElementById('alien4')
+
+      if(label=="Background_Noise")
+      {
+      img1.src="aliens-01.gif"
+      img2.src="aliens-02.png"
+      img3.src="aliens-03.png"
+      img4.src="aliens-04.png"
+    }
+
+      
+      else if(label=="Clap")
+      {
+      img1.src="aliens-01.png"
+      img2.src="aliens-02.gif"
+    img3.src="aliens-03.png"
+      img4.src="aliens-04.png"
+      }
+
+      else if(label=="Thumping")
+      {
+      img1.src="aliens-01.png"
+      img2.src="aliens-02.png"
+    img3.src="aliens-03.gif"
+      img4.src="aliens-04.png"
+      }
+
+      else(label=="Clap")
+      {
+      img1.src="aliens-01.png"
+      img2.src="aliens-02.png"
+    img3.src="aliens-03.png"
+    img4.src="aliens-04.gif"
+      }
+    
+
+      
+ 
+
+
+    }
